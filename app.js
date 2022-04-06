f62dd818298bfc7697672ef2b1f22f5a21aaecc8
Developer-Dennis <denniskipkogei11@gmail.com>
2022-04-06 12:05:06 +0300

    added authentication to tidings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import mysql from 'mysql';
-import bcrypt from 'bcrypt';
-import session from 'express-session' ;
+const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const app = express();
 
@@ -10,6 +10,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tidings'
+});
+connection.query(
+    'SELECT * FROM users',(error,results) => {
+        if(error) console.log(error)
+        console.log('connection successfully')
+    }
+)
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -18,6 +30,87 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about-us.ejs')
 })
+
+
+app.get('/signup', (req,res) => {
+    res.render('signup.ejs',{error: false})
+})
+
+
+app.post('/signup', (req,res) => {
+    let email = req.body.email,
+    fullname = req.body.fullname,
+    gender = req.body.gender,
+    password = req.body.password,
+    confirmpassword = req.body.confirmpassword
+
+    if(password = confirmpassword) {
+        bcrypt.hash(password, 10, (error, hash) => {
+            connection.query(
+                'SELECT email FROM users WHERE email = ?',
+                [email],
+                (error,results) => {
+                    
+                    if(results.length === 0){
+                        connection.query(
+                            'INSERT INTO users (email, fullname,gender, password) VALUES(?,?,?,?)',
+                            [email, fullname, gender, hash],
+                            (error, results) => {
+                                res.redirect('/login');
+                            }
+                        )
+                    
+                    } else{
+                        console.log('email already registered')
+                    }
+                 })         
+           })
+         
+        } else{
+            console.log('enter macthing passwords')
+        }
+        
+             
+    })
+    
+
+
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+
+
+app.post('/login',(req,res)=>{
+    let email = req.body.email
+    let password = req.body.password
+
+    connection.query(
+        'SELECT * FROM users WHERE email=?',[email],
+        (error,results)=>{
+            if(results.length>0){
+                bcrypt.compare(password,results[0].password, (error,isEqual)=>{
+                    if(isEqual){
+                        
+                        res.render('about-us.ejs')
+
+                    }else{
+                      console.log('mmn')
+                        
+                       
+                    }
+                })
+                
+            }else{
+                console.log('str')
+            }
+        }
+    )
+})
+
+
 
 
 const PORT = process.env.PORT || 3000
