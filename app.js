533426d5d15de2049b0d5a8090a0be379c0d43f3
Developer-Dennis <denniskipkogei11@gmail.com>
2022-04-07 11:32:05 +0300

    Edited my views,app.js and my login and signup

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-const express = require('express');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
+const express = require('express')
+const mysql = require('mysql')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
 
-const app = express();
-
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+const app = express()
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'tidings'
-});
-
-
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    // cookie: { secure: true }
-  }))
-  app.use((req, res, next) => {
-    if(req.session.userId === undefined){
-        res.locals.isLoggedIn = false;
-        
-    } else {
-        res.locals.isLoggedIn = true;
-        res.locals.fullname = req.session.fullname;
-    }
-    next();
 })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
 
-
-
-
-
-connection.query(
-    'SELECT * FROM users',(error,results) => {
-        if(error) console.log(error)
-        console.log('connection successfully')
-    }
-)
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -55,111 +25,92 @@ app.get('/about', (req, res) => {
     res.render('about-us.ejs')
 })
 
-
-app.get('/signup', (req,res) => {
-    res.render('signup.ejs',{error: false})
-})
-
-
-app.post('/signup', (req,res) => {
-    let email = req.body.email,
-    fullname = req.body.fullname,
-    gender = req.body.gender,
-    password = req.body.password,
-    confirmpassword = req.body.confirmpassword
-
-    if(password = confirmpassword) {
-        bcrypt.hash(password, 10, (error, hash) => {
-            connection.query(
-                'SELECT email FROM users WHERE email = ?',
-                [email],
-                (error,results) => {
-                    
-                    if(results.length === 0){
-                        connection.query(
-                            'INSERT INTO users (email, fullname,gender, password) VALUES(?,?,?,?)',
-                            [email, fullname, gender, hash],
-                            (error, results) => {
-                                res.redirect('/login');
-                            }
-                        )
-                    
-                    } else{
-                        console.log('email already registered')
-                    }
-                 })         
-           })
-         
-        } else{
-            console.log('enter macthing passwords')
-        }
-        
-             
-    })
-    
-
-
-
-
 app.get('/login', (req, res) => {
-    res.render('login.ejs')
+    let user = {
+        email: '',
+        password: ''
+    }
+    res.render('login.ejs', {error: false, user: user})
 })
 
-
-
-app.post('/login',(req,res)=>{
-    let email = req.body.email
-    let password = req.body.password
+app.post('/login', (req, res) => {
+    let user = {
+        email: req.body.email,
+        password: req.body.password
+    }
 
     connection.query(
-        'SELECT * FROM users WHERE email=?',[email],
-        (error,results)=>{
-            if(results.length>0){
-                bcrypt.compare(password,results[0].password, (error,isEqual)=>{
-                    if(isEqual){
-                        req.session.userId = results[0].id;
-                       req.session.fullname = results[0].fullname;
-                        res.render('new-tyd.ejs')
-
-
-                    }else{
-                      console.log('mmn')
-                        
-                       
+        'SELECT * FROM users WHERE email = ?', [user.email],
+        (error, results) => {
+            if (results.length > 0) {
+                bcrypt.compare(user.password, results[0].password, (error, isEqual) => {
+                    if(isEqual) {
+                        res.redirect('/')
+                    } else {
+                        let message = 'Email/Password mistmatch.'
+                        res.render('login.ejs', {error: true, message: message, user: user})
                     }
                 })
-                
-            }else{
-                console.log('str')
+
+            } else {
+                let message = 'Account does not exist. Please create one'
+                res.render('login.ejs', {error: true, message: message, user: user})
             }
         }
-    )
+    )    
+
+
 })
 
-
-app.get('/tyd',(req, res) => {
-    if (res.locals.isLoggedIn)
-    connection.query(
-        'SELECT * FROM tyds WHERE userID =?',[req.session.userID],(error,results)=>{
-            res.render('new-tyd.ejs')
-        }
-    )
-    
+app.get('/signup', (req, res) => {
+    let user = {
+        email: '',
+        fullname: '',
+        gender: '',
+        password: '',
+        confirmPassword: ''
+    }
+    res.render('signup.ejs', {error:false, user: user})
 })
 
+app.post('/signup', (req, res) => {
+    let user = {
+        email: req.body.email,
+        fullname: req.body.fullname,
+        gender: req.body.gender,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
 
-app.post('/tyd',(req,res) => {
-    connection.query(
-        'INSERT INTO tyds (tyd, userID) VALUES (?,12)',
-         [req.body.tyd],
-         (error, results) => {
-             console.log(error)
-             res.render('about-us.ejs');
-         }
-    );
- });
- 
+    if(user.password === user.confirmPassword) {
+        connection.query(
+            'SELECT email FROM users WHERE email = ?', [user.email],
+            (error, results) => {
+                if(results.length === 0) {
+                    bcrypt.hash(user.password, 10, (error, hash) => {
+                        connection.query(
+                            'INSERT INTO users (email, fullname, gender, password) VALUES (?,?,?,?)',
+                            [user.email, user.fullname, user.gender, hash],
+                            (error, results) => {
+                                res.redirect('/')
+                            }
+                        )
+                    })
+                } else {
+                    let message = 'Email already exists.'
+                    res.render('signup.ejs', {error: true, message: message, user: user})
+                }
+            }
+        )
+    } else {
+        let message = 'Password & Confirm Password do not match.'
+        console.log(user)
+        res.render('signup.ejs', {error: true, message: message, user: user})
+    }
+
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server up on PORT ${PORT}`);
+    console.log(`Server up on PORT ${PORT}`)
 })
